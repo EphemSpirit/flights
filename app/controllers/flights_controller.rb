4665a7f8478d2793ba581b8f37f97aa26cdc1340
Drew Hund <languageinvestigator@gmail.com>
2020-09-06 12:55:14 -0400

    fixed flight params issue

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,13 +5,16 @@ class FlightsController < ApplicationController
     @dates = Flight.all.map{ |x| [x.departure] }
     @passengers = [[1], [2], [3], [4]]
 
-    @results = Flight.where(["start = ? AND finish = ? AND departure = ?", flight_params[:from_airport], flight_params[:to_airport], flight_params[:departure]])
+    if params[:flight]
+      @results = Flight.where(["start = ? AND finish = ? AND departure = ?", flight_params[:from_airport], flight_params[:to_airport], flight_params[:departure]])
+    end
+
   end
 
   private
 
     def flight_params
-      params.require(:flight).permit(:from_airport, :to_airport, :departure)
+      params.require(:flight).permit(:from_airport, :to_airport, :departure, :passengers)
     end
 
 end
