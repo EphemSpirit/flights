c51f6ac0919c3c4b64c63174656f542b43f34312
Drew Hund <languageinvestigator@gmail.com>
2021-03-07 14:07:51 -0500

    styling done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,12 @@
 class FlightsController < ApplicationController
+  include FlightsHelper
 
   def index
     @airports = Airport.all.map{ |x| [x.code, x.id] }
     @dates = Flight.all.map{ |x| [x.departure.strftime("%Y-%m-%d")] }
     @passengers = [[1], [2], [3], [4]]
 
-    if params[:flight]
+    if available_flights?
       @results = Flight.where(["start = ? AND finish = ? AND departure = ?", flight_params[:from_airport], flight_params[:to_airport], flight_params[:departure]])
       @num_pass = flight_params[:passengers]
       @booking = Booking.new
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,9 @@
 module FlightsHelper
+  def available_flights?
+    !params[:flight].nil?
+  end
+
+  def no_results?
+    params[:flights] && !@results.any?
+  end
 end
