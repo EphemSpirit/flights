e80d28bdd30b0458424108b83507dc56a3ab1598
Drew Hund <languageinvestigator@gmail.com>
2021-04-13 18:52:21 -0400

    cleaned up db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-
+Flight.destroy_all
 
 airports = ["SFO", "LAX", "BOS", "LGA", "JFK", "RLA", "DFW"]
 
@@ -6,9 +6,20 @@ airports.each do |airport|
   Airport.create(code: airport)
 end
 
-100.times do
-  Flight.create!(from_airport: Airport.all.sample,
-                to_airport: Airport.all.sample,
-                departure: (rand(1..30).days.from_now),
-                duration: rand(60..500).to_s)
-end
+# 100.times do
+#   Flight.create!(from_airport: Airport.all.sample,
+#                 to_airport: Airport.all.sample,
+#                 departure: (rand(1..30).days.from_now),
+#                 duration: rand(60..500).to_s)
+# end
+
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 5.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 3.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 10.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 12.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 14.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 20.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 30.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 15.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 25.days.from_now, duration: "120")
+Flight.create!(from_airport: Airport.all.sample, to_airport: Airport.all.sample, departure: 35.days.from_now, duration: "120")
